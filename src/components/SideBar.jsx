81715a173d3ef36d8a706e58048bff6923f11efb
Filippo-Timo <filtimo2002@gmail.com>
2025-12-05 16:55:32 +0100

    Finishes project

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,8 +4,8 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const SideBar = () => {
     return (
         <div className="d-flex flex-column text-white d-none d-md-block h-100vh w-25" style={{ backgroundColor: "#2a2a2a" }}>
-            <div className="d-flex align-items-center pt-3 ps-3">
-                <img src="../public/logos/music.svg" type="svg" alt="Logo Apple music" className="img-fluid" />
+            <div className="d-flex align-items-center py-3 ps-3">
+                <img src="../public/logos/music.svg" type="svg" alt="Logo Apple music" className="img-fluid py-2" />
             </div>
             <div className="p-3">
                 <input type="search" placeholder="Cerca" className="w-100"></input>
@@ -18,7 +18,7 @@ const SideBar = () => {
                     <i className="bi bi-music-note-list text-danger"></i> Novità
                 </ListGroup.Item>
                 <ListGroup.Item action href="#radio" className="text-white border-0 w-100" style={{ backgroundColor: "#2a2a2a" }}>
-                    <i class="bi bi-broadcast text-danger"></i> Radio
+                    <i className="bi bi-broadcast text-danger"></i> Radio
                 </ListGroup.Item>
             </ListGroup>
         </div >
